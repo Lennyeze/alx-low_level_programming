e19040870d98b50974def5dc36dab9a5b3f77101
Lennyeze <leonardezeribi88@gmail.com>
2022-10-19 15:11:54 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: This function prints the alphabets in lowercase
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_alphabet(void)
+
+/*
+ * print_alphabet - the for loop is used to run through the alphabet
+ *
+ * from a to z
+ *
+ */
+
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+{
+_putchar('\n');
+}
+}
