28f2a805f27365fb803d499682efb8579850c072
Lennyeze <leonardezeribi88@gmail.com>
2022-11-05 14:51:37 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,110 +1,37 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * idx_num_starts - find index where a digit is first found in string
- * @s: string to search
- * Return: integer index where digit is first found
- */
-
-int idx_num_starts(char *s)
-{
-	int i;
-
-	for (i = 0; i < _strlen(s); i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			return (i);
-	}
-	return (-1);
-}
-
-/**
- * find_sign - determine if integer is negative
+ * _atoi - converts a string to an integer.
  *
- * @s: integer
+ * @s: the string to convert
  *
- * Return: integer 1 or -1
- *
- */
-
-int find_sign(char *s)
-{
-	int negatives = 0, i = 0, sign = 1;
-
-	while (i < (idx_num_starts(s)))
-	{
-		if (s[i++] == '-')
-			negatives++;
-	}
-
-	if (negatives % 2 != 0)
-		sign = -1;
-
-	return (sign);
-}
-
-/**
- * _atoi - convert string to int
- *
- * @s: string to convert
- *
- * Return: integer
+ * Return: the converted string.
  *
  */
 
 int _atoi(char *s)
 {
+	short boolean;
+	int i, minus, result;
 
-	int idx_digit_starts = (idx_num_starts));
-	int sign;
-	int digits_to_print = 0;
-	int t = 1, i;
-	unsigned int num = 0;
-	int digit = (idx_num_starts(s));
+	i = minus = result = boolean = 0;
+	minus = -1;
 
-	if (idx_digit_starts < 0)
-		return (0);
-
-	sign = find_sign(s);
-
-	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9')
-			&& (idx_digit_starts <= _strlen(s)))
+	while (s[i] != '\0')
 	{
-		digits_to_print += 1;
-		idx_digit_starts++;
-	}
+		if (s[i] == '-')
+			minus *= -1;
 
-	i = 1;
-	while (i < digits_to_print)
-	{
-		t *= 10;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
+		}
+		else if (boolean == 1)
+			break;
 		i++;
 	}
-
-	for (i = digit; i < (digit + digits_to_print); i++)
-	{
-		num += (s[i] - '0') * t;
-		t /= 10;
-	}
-
-	return (num * sign);
+	result *= minus;
+	return (result);
 }
