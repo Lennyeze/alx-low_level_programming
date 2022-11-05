aed42c4b96c844386b8366d7945711a4acb543b7
Lennyeze <leonardezeribi88@gmail.com>
2022-11-05 14:33:31 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -36,3 +36,73 @@ int idx_num_starts(char *s)
 	}
 	return (-1);
 }
+
+/**
+ * find_sign - determine if integer is negative
+ *
+ * @s: integer
+ *
+ * Return: integer 1 or -1
+ *
+ */
+
+int find_sign(char *s)
+{
+	int negatives = 0, i = 0, sign = 1;
+
+	while (i < (idx_num_starts(s)))
+	{
+		if (s[i++] == '-')
+			negatives++;
+	}
+
+	if (negatives % 2 != 0)
+		sign = -1;
+	return (sign);
+}
+
+/**
+ * _atoi - convert string to int
+ *
+ * @s: string to convert
+ *
+ * Return: integer
+ *
+ */
+
+int _atoi(char *s)
+{
+	int idx_digit_starts = (idx_num_starts));
+	int sign;
+	int digits_to_print = 0;
+	int t = 1, i;
+	unsigned int num = 0;
+	int digit = (idx_num_starts(s));
+
+	if (idx_digit_starts < 0)
+		return (0);
+
+	sign = find_sign(s);
+
+	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9')
+			&& (idx_digit_starts <= _strlen(s)))
+	{
+		digits_to_print += 1;
+		idx_digit_starts++;
+	}
+
+	i = 1;
+	while (i < digits_to_print)
+	{
+		t *= 10;
+		i++;
+	}
+
+	for (i = digit; i < (digit + digits_to_print); i++)
+	{
+		num += (s[i] - '0') * t;
+		t /= 10;
+	}
+
+	return (num * sign);
+}
